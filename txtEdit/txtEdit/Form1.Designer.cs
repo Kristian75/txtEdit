cb93c080a004fbe1b453578cbd0657390f6b7ac7
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2024-04-23 11:25:18 +0200

    Add files via upload

---
+++ txtEdit/txtEdit/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace txtEdit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnOpen = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnNew = new System.Windows.Forms.Button();
+            this.BtnUnder = new System.Windows.Forms.Button();
+            this.BtnItalic = new System.Windows.Forms.Button();
+            this.BtnBold = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 101);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 273);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnOpen
+            // 
+            this.BtnOpen.Location = new System.Drawing.Point(12, 45);
+            this.BtnOpen.Name = "BtnOpen";
+            this.BtnOpen.Size = new System.Drawing.Size(128, 50);
+            this.BtnOpen.TabIndex = 3;
+            this.BtnOpen.Text = "Open";
+            this.BtnOpen.UseVisualStyleBackColor = true;
+            this.BtnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(173, 45);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(128, 50);
+            this.BtnSave.TabIndex = 4;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnNew
+            // 
+            this.BtnNew.Location = new System.Drawing.Point(342, 45);
+            this.BtnNew.Name = "BtnNew";
+            this.BtnNew.Size = new System.Drawing.Size(128, 50);
+            this.BtnNew.TabIndex = 5;
+            this.BtnNew.Text = "New File";
+            this.BtnNew.UseVisualStyleBackColor = true;
+            this.BtnNew.Click += new System.EventHandler(this.BtnNew_Click);
+            // 
+            // BtnUnder
+            // 
+            this.BtnUnder.Location = new System.Drawing.Point(748, 52);
+            this.BtnUnder.Name = "BtnUnder";
+            this.BtnUnder.Size = new System.Drawing.Size(40, 36);
+            this.BtnUnder.TabIndex = 6;
+            this.BtnUnder.Text = "U";
+            this.BtnUnder.UseVisualStyleBackColor = true;
+            // 
+            // BtnItalic
+            // 
+            this.BtnItalic.Location = new System.Drawing.Point(702, 52);
+            this.BtnItalic.Name = "BtnItalic";
+            this.BtnItalic.Size = new System.Drawing.Size(40, 36);
+            this.BtnItalic.TabIndex = 7;
+            this.BtnItalic.Text = "/";
+            this.BtnItalic.UseVisualStyleBackColor = true;
+            // 
+            // BtnBold
+            // 
+            this.BtnBold.Location = new System.Drawing.Point(656, 52);
+            this.BtnBold.Name = "BtnBold";
+            this.BtnBold.Size = new System.Drawing.Size(40, 36);
+            this.BtnBold.TabIndex = 8;
+            this.BtnBold.Text = "B";
+            this.BtnBold.UseVisualStyleBackColor = true;
+            this.BtnBold.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnBold);
+            this.Controls.Add(this.BtnItalic);
+            this.Controls.Add(this.BtnUnder);
+            this.Controls.Add(this.BtnNew);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.BtnOpen);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnOpen;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnNew;
+        private System.Windows.Forms.Button BtnUnder;
+        private System.Windows.Forms.Button BtnItalic;
+        private System.Windows.Forms.Button BtnBold;
+    }
+}
+
---
+++ txtEdit/txtEdit/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace txtEdit
+{
+    public partial class Form1 : Form
+    {
+        public object CreateDirectory { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            Stream st;
+            OpenFileDialog d1 = new OpenFileDialog();  
+            if (d1.ShowDialog() == DialogResult.OK)
+            {
+                if ((st = d1.OpenFile()) !=null)
+                {
+                    string file = d1.FileName;
+                    String str = File.ReadAllText(file);
+                    richTextBox1.Text = str;
+                }
+            }
+
+        }
+
+        private void BtnNew_Click(object sender, EventArgs e) => CreateDirectory(new);
+
+        {
+            StreamWriter tw = new StreamWriter(@"C:\Users\Admin\Desktop\Hello.txt");
+            tw.Write(richTextBox1);
+                richTextBox1.Text = "file uspješno spremljen";
+                Random r = new Random();
+                richTextBox1.ForeColor = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), 0);
+                tw.Close();
+        }
+    }
+}
